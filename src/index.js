d0b48ace359ebdc7ab724da936eaeeb2d28d4a56
shadskii <jake.k.hassel@gmail.com>
2018-06-04 00:04:23 -0400

    Adding some colors

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ const fullpage = require("fullpage.js");
 const $ = require('jquery');
 
 $(document).ready(function () {
-    $('#fullpage').fullpage();
+    $('#fullpage').fullpage({
+        sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+        anchors: ['section0', 'section1,', 'section2', 'section3'],
+    });
 });
