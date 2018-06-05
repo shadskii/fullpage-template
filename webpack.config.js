567652a58506d87bed929cb0b94436f2b87daf0a
shadskii <jake.k.hassel@gmail.com>
2018-06-05 00:02:30 -0400

    Adding separate vendor bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
-const path = require('path')
+const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     target: 'web',
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     devServer: {
         contentBase: path.join(__dirname, "dist"),
         port: 9000
     },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: { test: /[\\/]node_modules[\\/]/, name: "vendors", chunks: "all" }
+            }
+        }
+    },
     plugins: [
+        new CleanWebpackPlugin(['dist']),
         new CopyWebpackPlugin([
             {
                 from: 'src/index.html',
