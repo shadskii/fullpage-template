c390803c1d63c01aa450c3926c674f0bb9b954a6
shadskii <jake.k.hassel@gmail.com>
2018-06-06 23:35:32 -0400

    Scrolling smoohthly now

--- src/index.js
+++ src/index.js
@@ -1,9 +1,39 @@
 const fullpage = require("fullpage.js");
 const $ = require('jquery');
+// import 'fullpage.js'
 
 $(document).ready(function () {
     $('#fullpage').fullpage({
-        sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
-        anchors: ['section0', 'section1,', 'section2', 'section3'],
+        sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', '#7BAAB3'],
+        anchors: ['section0', 'section1', 'section2', 'section3'],
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 1000,
+        autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        continuousHorizontal: false,
+        scrollHorizontally: false,
+        interlockedSlides: false,
+        dragAndMove: false,
+        offsetSections: false,
+        resetSliders: false,
+        fadingEffect: false,
+        scrollOverflow: false,
+        scrollOverflowReset: false,
+        scrollOverflowOptions: null,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+        bigSectionsDestination: null,
+
+
     });
 });
