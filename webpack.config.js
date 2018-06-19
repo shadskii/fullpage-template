2364bc9b481d8ac893320cefc0d3c14571548a74
shadskii <jake.k.hassel@gmail.com>
2018-06-19 18:56:20 -0400

    Using html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,50 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    target: 'web',
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+  entry: './src/index.js',
+  target: 'web',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 9000,
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
     },
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        port: 9000
-    },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                commons: { test: /[\\/]node_modules[\\/]/, name: "vendors", chunks: "all" }
-            }
-        }
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new CopyWebpackPlugin([
-            {
-                from: 'src/index.html',
-                to: 'index.html',
-                toType: 'file'
-            },
-            {
-                from: 'src/styles.css',
-                to: 'styles.css',
-                toType: 'file'
-            },
-            {
-                from: 'static'
-            }
-        ])
-    ]
-}
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new CopyWebpackPlugin([
+      {
+        from: 'src/index.html',
+        to: 'index.html',
+        toType: 'file',
+      },
+      {
+        from: 'src/styles.css',
+        to: 'styles.css',
+        toType: 'file',
+      },
+      {
+        from: 'static',
+      },
+    ]),
+    new HtmlWebpackPlugin({
+      title: 'fullpage template',
+      template: 'src/index.html',
+    }),
+  ],
+};
