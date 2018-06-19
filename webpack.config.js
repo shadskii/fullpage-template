43311619daf8e89a68c9d687e88cb4203333876d
shadskii <jake.k.hassel@gmail.com>
2018-06-18 23:19:34 -0400

    Static file handling

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,9 @@ module.exports = {
                 from: 'src/styles.css',
                 to: 'styles.css',
                 toType: 'file'
+            },
+            {
+                from: 'static'
             }
         ])
     ]
